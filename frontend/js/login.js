d7eb36b8ff59593d04a07d7eff63524690e778e9
Vitor Garcia <garciavitor159@gmail.com>
2025-09-05 03:29:32 -0300

    Commit de nova versão.
    - Modificações:
    
    - Adicionado páginas de login, validação de pergunta de segurança e alteração de senha.

---
+++ frontend/js/login.js
@@ -0,0 +1,64 @@
+// Variáveis e funções importadas
+import {
+  form,
+  email,
+  senha,
+  adicionarEventoSubmit,
+  limparVal,
+  validarEmail,
+  exibirErrTela,
+  validarSenha,
+  enviarDados,
+  redirecionar,
+  alternarModalMsgs,
+  limparCampo,
+  limparCampos,
+} from "./utils.js";
+
+adicionarEventoSubmit(form, async (e) => {
+  e.preventDefault();
+  const valEmail = limparVal(email.value);
+  const valSenha = limparVal(senha.value);
+
+  if (!validarEmail(email, valEmail)) {
+    exibirErrTela(
+      "Erro: O e-mail é obrigatório, deve ser válido e conter até 80 caracteres.",
+      email
+    );
+
+    return;
+  }
+
+  if (!validarSenha(senha, valSenha)) {
+    exibirErrTela(
+      "Erro: A senha é obrigatória e deve conter entre 8 e 30 caracteres, incluindo uma letra minúscula, uma letra maiúscula, um número e um símbolo.",
+      senha
+    );
+
+    return;
+  }
+
+  const params = { acao: "efetuar_login", email: valEmail, senha: valSenha };
+  const url = "efetuar_login.php";
+  const metodo = "POST";
+  const { sucesso, dados, msg } = await enviarDados(params, url, metodo);
+
+  if (sucesso) {
+    redirecionar(msg, "painel.html");
+    return;
+  }
+
+  const { campoErr } = dados;
+  alternarModalMsgs(true, msg);
+
+  switch (campoErr) {
+    case "email":
+      limparCampo(email);
+      break;
+    case "senha":
+      limparCampo(senha);
+      break;
+    default:
+      limparCampos([email, senha]);
+  }
+});
